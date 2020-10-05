b68194f728281c233e61335c4c0d19e91b597946
Margus <margus.burk@gmail.com>
2020-10-05 06:26:37 +0300

    midagi

--- virus.py
+++ virus.py
@@ -1,3 +1,13 @@
 class koroona:
     def __init__(self):
         pass
+
+def kList(mList): 
+	 
+	r = 1
+	for x in mList: 
+		r = r * x 
+	return r
+l = int(input("sisestage haigete arv:"))
+list1 = [l, 3]  
+print(kList(list1))
