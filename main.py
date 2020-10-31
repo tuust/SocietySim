3160bd4a3a541569d4075c89107c04a6d6363e48
Margud1 <70514215+Margud1@users.noreply.github.com>
2020-10-31 23:12:32 +0200

    Update main.py
    Versioon 1.0

--- main.py
+++ main.py
@@ -3,56 +3,82 @@ import person as p
 import numpy as np
 import math
 from random import randint
-#inimene = int(input("sisestage inimeste arv:"))
-#haige = int(input("sisestage haigete arv:"))
-#surnud = int(input("sisestage suremisprotsent:"))
-#surnud = round(haige / 100)
-#haige = haige - l2
-#terved = inimene - haige
-#lo = [ka, l3, l2]
-#print(terved)
-##haige = (haige * 2)
+from itertools import cycle
+from pyglet import clock
+
 maju = 100
 majad = np.array([])
 
-koole = int(maju * 0.02)
-majad = np.append(majad, np.full((1,koole), b.kool()))
+# koole = int(maju * 0.02)
+# majad = np.append(majad, np.full((1,koole), b.kool()))
 
-haiglad = int(maju * 0.01)
-majad = np.append(majad, np.full((1,haiglad), b.haigla()))
+# haiglad = int(maju * 0.01)
+# majad = np.append(majad, np.full((1,haiglad), b.haigla()))
 
-kodu = maju - haiglad - koole
+# pood = int(maju * 0.02)
+# majad = np.append(majad, np.full((1,poed), b.pood()))
+
+
+kodu = maju #- haiglad - koole# - pood - kontor
 majad = np.append(majad, np.full((1,kodu), b.kodu()))
 
-# ja teiste majadega ka
 
 
 # teeb majad mitmedimensiooniliseks ja ajab majad segamini
 np.random.shuffle(majad)
 majad = np.reshape(majad, (int(math.sqrt(maju)),int(math.sqrt(maju))))
-
 #print(majad)
 #inimesed
+inimesi = kodu*3 #populatsiooni arvutamine
 
-inimesed = {'vanur':[],}
-inimesed = {'tooline':[],}
-inimesed = {'tootu':[],}
-inimesed = {'opilane':[],}
+haige = 3
+terved = inimesi - haige
+
+inimesed = []
+pikkus = math.sqrt(maju)
 for rida in range(len(majad)):
     for maja in range(len(majad[rida])):
         if isinstance(majad[rida, maja], b.kodu):
-            print('kodu')
             range(len(majad))
-            inimene = p.vanur()
-            inimene.kodu = [rida, maja]#koordinaadid
-            inimene.asukoht = [rida, maja]
-            inimesed['vanur'].append(inimene)
-            inimene = p.tootu()
-            inimesed['tootu'].append(inimene)
-            inimene = p.tooline()
-            inimesed['tooline'].append(inimene)
-            inimene = p.opilane()
-            inimesed['opilane'].append(inimene)
+            if len(inimesed) < kodu-5:
+                inimarv = randint(2,5)#Mitu inimest elab ühes kodus
+            else:
+                inimarv = kodu - len(inimesed)
+            for inim in range(inimarv):
+                
+                # inimese loomine
+                inim = p.tooline()
+                inim.staatus = 0    #Terved(0), Haiged(1), Tervenenud(2)
+                inim.kodu = [rida, maja] #Inimese kodu kordinaadid
+                i = math.sqrt(maju)
+                inim.too = [randint(0,pikkus),randint(0,pikkus)]
+                inim.pood = [randint(0,pikkus),randint(0,pikkus)]
+                inim.asukoht = [rida, maja] #Kus inimene asub
+                inimesed.append(inim)
 
+# Algsete haigete määramine
+koords = []
+def inithaige():
+    x = randint(0,pikkus)
+    if x not in koords:
+        koords.append(x)
+        inimesed[x].staatus = 1
+    else: inithaige()
+for i in range(haige):
+    inithaige()
 
-
+kohad = cycle(iter(['too', 'pood', 'kodu']))
+while True:
+    haiged = []
+    koht = next(kohad)
+    for inim in inimesed:
+        inim.asukoht = getattr(inim, koht) 
+        if inim.staatus == 1:
+            haiged.append(inim.asukoht)
+        
+    for inim in inimesed:
+        if randint(0,100) <= 10: #tervenemise protsent
+            inim.staatus = 2
+        if inim.asukoht in haiged:
+            if randint(0,100) <= 40 and inim.staatus != 2: #haigestumise protsent
+                inim.staatus = 1
